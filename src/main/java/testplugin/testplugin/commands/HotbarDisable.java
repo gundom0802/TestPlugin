1a19e754b8d1bf24674e59f43610074f2cdc1b6e
3clipse <gundom0802@gmail.com>
2023-09-06 21:18:06 -0400

    Allowed command senders to enable and disable hotbarlock for other players

--- src/main/java/testplugin/testplugin/commands/HotbarDisable.java
+++ src/main/java/testplugin/testplugin/commands/HotbarDisable.java
@@ -1,28 +1,38 @@
 package testplugin.testplugin.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import testplugin.testplugin.util.HotbarSetUtil;
 
 public class HotbarDisable implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String [] args) {
-        if(!(sender instanceof Player)) {
-            sender.sendMessage("Only players can run this command");
+        if(!(sender instanceof ConsoleCommandSender || sender instanceof Player)) {
+            sender.sendMessage("Only humans can run this command");
             return true;
 
         }
+        if (args.length != 1) {
+            return false;
+        }
 
-        Player pl = (Player) sender;
+        Player pl = Bukkit.getPlayer(args[0]);
+        if (pl == null) {
+            sender.sendMessage("This player does not exist");
+            return true;
+        }
+
 
         if (!(HotbarSetUtil.getSet(pl.getUniqueId().toString()))) {
-            pl.sendMessage("You already have hotbarlock disabled");
+            Bukkit.getLogger().info(args[0] + "already has hotbarlock disabled");
         }
         else {
             HotbarSetUtil.removeSet(pl.getUniqueId().toString());
-            pl.sendMessage("Hotbarlock disabled");
+            Bukkit.getLogger().info("Hotbarlock disabled for " + args[0]);
         }
         return true;
 
